3ee75f1a05f494455b5eefbecc0cefcf93af01a6
Artem <arty.lindemann@gmail.com>
2023-01-24 00:51:30 +0600

    a

--- dashboard/user/static/user/js/index.js
+++ dashboard/user/static/user/js/index.js
@@ -1,21 +1,22 @@
-$(document).ready(function(){
-$('.delete').on('click', function(){
+$(document).ready(function (this) {
+  $('.delete').on('click', function () {
     var _user_id = $(this).data('answer');
     $.ajax({
       url: "/delete",
       type: "get",
       data: {
-        'user_id':_user_id,
+        'user_id': _user_id,
       },
       dataType: "json",
       success: function (res) {
-        if(res.bool=!false){
+        if (res.bool = !false) {
           $this.parents(".content").remove();
-        }else if(res.bool=!false){
+        } else if (res.bool = !false) {
           alert(res.message);
-        }else{
-          alert(res.message);}
+        } else {
+          alert(res.message);
         }
+      }
     });
   });
 });
--- dashboard/user/urls.py
+++ dashboard/user/urls.py
@@ -1,10 +1,10 @@
-
-from .views import *
+from django.urls import path
+from .views import profile, LoginUser, add_user, edit, delete
 from django.contrib.auth.views import LogoutView
 
 urlpatterns = [
     path('', LoginUser.as_view(), name='login'),
-    path('logout', LogoutView.as_view(next_page='login')),
+    path('logout', LogoutView.as_view(next_page='login'), name="logout"),
     path('dashboard', profile, name='profile'),
     path('add_user', add_user, name='add_user'),
     path('edit/<int:pk>/', edit, name='edit'),

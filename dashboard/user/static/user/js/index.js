cf890f58224b4edfeebc150ded52925f53489f1d
Artem <arty.lindemann@gmail.com>
2023-01-24 03:48:16 +0600

    Update index.js

--- dashboard/user/static/user/js/index.js
+++ dashboard/user/static/user/js/index.js
@@ -1,6 +1,6 @@
 $(document).ready(function () {
   $('.delete').on('click', function () {
-    var _user_id = $(this).data('answer');
+    const _user_id = $(this).data('answer');
     $.ajax({
       url: "/delete",
       type: "get",

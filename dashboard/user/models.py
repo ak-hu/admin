6101ea423bd0ca60272343f47d0ef6b25c1d6fe7
Artem <arty.lindemann@gmail.com>
2023-01-25 02:49:29 +0600

    Update models.py

--- dashboard/user/models.py
+++ dashboard/user/models.py
@@ -12,5 +12,5 @@ class Main(models.Model):
     phone = models.CharField(max_length=30)
     password = models.CharField(max_length=30)
 
-    def __str__():
+    def __str__(self):
         return this.username

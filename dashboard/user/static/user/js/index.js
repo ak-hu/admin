3937fab0a984d8e65fd61df3cefd38f9b8d20eec
Artem <arty.lindemann@gmail.com>
2023-01-25 03:29:26 +0600

    Update index.js

--- dashboard/user/static/user/js/index.js
+++ dashboard/user/static/user/js/index.js
@@ -1,19 +1,19 @@
-  $('.delete').on('click', () => {
-    const $my_doc = $(this);
-    const _user_id = $(this).data('answer');
-    $.ajax({
-      url: "/delete",
-      type: "get",
-      data: {
-        'user_id': _user_id,
-      },
-      dataType: "json",
-      success: function (res) {
-        if (res.bool) {
-          $my_doc.parents(".content").remove();
-        } else {
-          alert(res.message);
-        }
+$('.delete').on('click', () => {
+  const $my_doc = $(this);
+  const _user_id = $(this).data('answer');
+  $.ajax({
+    url: "/delete",
+    type: "get",
+    data: {
+      'user_id': _user_id,
+    },
+    dataType: "json",
+    success: function (res) {
+      if (res.bool = ! false) {
+        $my_doc.parents(".content").remove();
+      } else {
+        alert(res.message);
       }
-    });
+    }
   });
+});

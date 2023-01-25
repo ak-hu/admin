ac16590c95f89f3f9290495e800dd32636e00b4e
Artem <arty.lindemann@gmail.com>
2023-01-26 01:31:05 +0600

    Update settings.py

--- dashboard/dashboard/settings.py
+++ dashboard/dashboard/settings.py
@@ -76,7 +76,9 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
-        'PASSWORD': '5.u8@sZv#+^[Aj3:',
+        'PASSWORD': '',
+        'HOST': '',       
+        'PORT': '', 
     }
 }
 
